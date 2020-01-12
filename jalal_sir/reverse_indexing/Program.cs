f449cc91bdcf3b7f242f84c9eee93c8b72499279
mdzobayer <muhammad.zobayer@selise.ch>
2020-01-12 10:41:45 +0600

    Add reverse index syntax

---
+++ jalal_sir/reverse_indexing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace reverse_indexing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse Indexing");
+
+           int [] list =  { 8, 3, 2 };
+
+            Console.WriteLine(list[1]);
+            Console.WriteLine(list[^2]);
+        }
+    }
+}
