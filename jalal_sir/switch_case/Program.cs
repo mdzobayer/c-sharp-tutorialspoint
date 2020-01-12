79273ecedf060e26682edd33d792c7b4a3d0c3a0
mdzobayer <muhammad.zobayer@selise.ch>
2020-01-12 10:33:08 +0600

    Add switch case example

---
+++ jalal_sir/switch_case/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Switch case example");
+
+            int i = -8;
+
+            switch (i)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("One or Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                default:
+                    Console.WriteLine("Other");
+                    break;
+            }
+        }
+    }
+}
