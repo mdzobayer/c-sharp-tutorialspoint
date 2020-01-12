3fc587d81fb45df2b4489527a0552e581b08d2be
mdzobayer <muhammad.zobayer@selise.ch>
2020-01-12 11:02:53 +0600

    Add Expression Bodied Members example

---
+++ jalal_sir/lambda_expression/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lambda_expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Expression Bodied");
+
+            Console.WriteLine(DoubleTheValue(4));
+            Console.WriteLine(doubleTheValue(4));
+        }
+
+        static int DoubleTheValue(int someValue) {
+            return someValue * 2;
+        }
+
+        static int doubleTheValue(int someValue) => someValue * 2;
+    }
+}
