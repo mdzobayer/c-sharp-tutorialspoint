c0f715e3e312becf19395f47d3213cb4b2afb614
mdzobayer <muhammad.zobayer@selise.ch>
2020-01-12 13:00:41 +0600

    Add lambdaa expression syntax

--- jalal_sir/lambda_expression/Program.cs
+++ jalal_sir/lambda_expression/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace lambda_expression
 {
@@ -10,9 +12,46 @@ namespace lambda_expression
 
             Console.WriteLine(DoubleTheValue(4));
             Console.WriteLine(doubleTheValue(4));
+
+
+            List<int> numbers = new List<int>() {36, 71, 12,
+                             15, 29, 18, 27, 17, 9, 34};
+
+            Console.Write("The list : ");
+            foreach (var value in numbers)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+
+            // Using lambda expression
+            // to calculate square of 
+            // each value in the list
+            var square = numbers.Select(x => x * x);
+
+            Console.Write("Squares : ");
+            foreach (var value in square)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+
+            // Using lambda expression
+            // to calculate square of 
+            // divisible by 3
+            List<int> divBy3 = numbers.FindAll(x => (x % 3) == 0);
+
+            Console.Write("Numbers Divisible by 3 : ");
+            foreach (var value in divBy3)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+
         }
 
-        static int DoubleTheValue(int someValue) {
+        static int DoubleTheValue(int someValue)
+        {
             return someValue * 2;
         }
 
