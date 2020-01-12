50084a408c5b3890b23ffadfdaabaabb10069fdd
mdzobayer <muhammad.zobayer@selise.ch>
2020-01-12 09:51:29 +0600

    Add jalal_sir topics

---
+++ jalal_sir/StringBuilder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace string_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StringBuilder Example");
+
+             StringBuilder sb = new StringBuilder("Hello ", 50);
+
+            Console.WriteLine(sb);
+
+            sb.Append("World!!");
+            Console.WriteLine(sb);
+            sb.AppendLine("Hello C#!");
+            Console.WriteLine(sb);
+            sb.AppendLine("This is new line.");
+
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ jalal_sir/nullable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nullable Data type");
+
+            // Define nullable type
+            Nullable < int > n = null;
+
+            Console.WriteLine(n.GetValueOrDefault());  
+
+            // Define nullable type
+            int? n1 = null;
+
+            Console.WriteLine(n1.GetValueOrDefault());  
+
+            int? n2 = 47;
+
+            Console.WriteLine(n2.GetValueOrDefault()); 
+
+        }
+    }
+}
